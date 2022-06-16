461b5fec103e5cb5ae9dad1ec5bbf89a41eb6a1f
Hadi El-Yakhni <hadiyakhni33@gmail.com>
2022-06-16 19:17:07 +0200

    only use []() for links and not <>, as the latter is causing a formatting problem when refreshing

--- src/marks/Link.ts
+++ src/marks/Link.ts
@@ -5,28 +5,6 @@ import Mark from "./Mark";
 
 const LINK_INPUT_REGEX = /\[([^[]+)]\((\S+)\)$/;
 
-function isPlainURL(link, parent, index, side) {
-  if (link.attrs.title || !/^\w+:/.test(link.attrs.href)) {
-    return false;
-  }
-
-  const content = parent.child(index + (side < 0 ? -1 : 0));
-  if (
-    !content.isText ||
-    content.text !== link.attrs.href ||
-    content.marks[content.marks.length - 1] !== link
-  ) {
-    return false;
-  }
-
-  if (index === (side < 0 ? 1 : parent.childCount - 1)) {
-    return true;
-  }
-
-  const next = parent.child(index + (side < 0 ? -2 : 1));
-  return !link.isInSet(next.marks);
-}
-
 export default class Link extends Mark {
   get name() {
     return "link";
@@ -146,15 +124,15 @@ export default class Link extends Mark {
   get toMarkdown() {
     return {
       open(_state, mark, parent, index) {
-        return isPlainURL(mark, parent, index, 1) ? "<" : "[";
+        return "[";
       },
       close(state, mark, parent, index) {
-        return isPlainURL(mark, parent, index, -1)
-          ? ">"
-          : "](" +
-              state.esc(mark.attrs.href) +
-              (mark.attrs.title ? " " + state.quote(mark.attrs.title) : "") +
-              ")";
+        return (
+          "](" +
+          state.esc(mark.attrs.href) +
+          (mark.attrs.title ? " " + state.quote(mark.attrs.title) : "") +
+          ")"
+        );
       },
     };
   }
