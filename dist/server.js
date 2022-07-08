6e9a3c38a855c85db7c00a55516f890a17e2657d
Hadi El-Yakhni <hadiyakhni33@gmail.com>
2022-07-08 20:48:15 +0200

    build

--- dist/server.js
+++ dist/server.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.externalHtmlOrMdToHtml = exports.parseMarkdown = exports.schema = void 0;
+exports.externalHtmlOrMdToHtml = exports.mdToHtml = exports.parseMarkdown = exports.schema = void 0;
 const prosemirror_model_1 = require("prosemirror-model");
 const ExtensionManager_1 = __importDefault(require("./lib/ExtensionManager"));
 const dictionary_1 = __importDefault(require("./dictionary"));
@@ -107,12 +107,13 @@ const mdToHtml = (document) => (markdown) => {
     const doc = exports.parseMarkdown(markdown);
     return serializeToHTML(document)(doc);
 };
+exports.mdToHtml = mdToHtml;
 const externalHtmlOrMdToHtml = (isHTML_ = domHelpers_1.isHTML, document_ = document) => (content) => {
     if (isHTML_(content)) {
         return serializeToHTML(document_)(parseHTML(document_)(content));
     }
     else {
-        return mdToHtml(document_)(content);
+        return exports.mdToHtml(document_)(content);
     }
 };
 exports.externalHtmlOrMdToHtml = externalHtmlOrMdToHtml;
