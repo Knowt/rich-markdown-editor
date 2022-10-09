be1935a92f8505098b012e2b42c305f4d1069293
Charlie <68297416+charles01pd2018@users.noreply.github.com>
2022-10-09 14:46:52 -0400

    Fix Highlighting Error
    Checks to see if the selection has text content in it before applying a highlight

--- src/commands/applyHighlight.ts
+++ src/commands/applyHighlight.ts
@@ -1,13 +1,16 @@
 import { toggleMark } from "prosemirror-commands";
 import { EditorState } from "prosemirror-state";
 import addMarkToSelection from "./addMarkToSelection";
+import isSelectionEmpty from '../queries/isSelectionEmpty';
 
 export default function applyHighlight(type) {
   return (state: EditorState, dispatch) => {
-    if (state.selection.empty) {
-      return toggleMark(type)(state, dispatch);
+    if ( !isSelectionEmpty( state.selection ) ) {
+      if (state.selection.empty) {
+        return toggleMark(type)(state, dispatch);
+      }
+  
+      return addMarkToSelection(type)(state, dispatch);
     }
-
-    return addMarkToSelection(type)(state, dispatch);
   };
 }
