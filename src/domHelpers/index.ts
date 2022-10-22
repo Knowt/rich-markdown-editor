aa1de9d1a9af32747874d5aa7cf59f07ca11e1db
Charlie <68297416+charles01pd2018@users.noreply.github.com>
2022-10-22 13:01:01 -0700

    Add isHTML check warning

--- src/domHelpers/index.ts
+++ src/domHelpers/index.ts
@@ -1,3 +1,10 @@
+/**
+ * Detects any html within note content.
+ * BE CAREFUL when using this function as a precursor to parsing
+ * note content as HTML since HTML can occur naturally within the note -
+ * i.e -> computer science notes
+ * This function is used for the backend quiz generation
+ */
 export const isHTML = (str) => {
   const doc = new DOMParser().parseFromString(str, "text/html");
   return Array.from(doc.body.childNodes).some((node) => node.nodeType === 1);
