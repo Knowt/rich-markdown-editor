64bf37cb840b00391d35c772f68bce78dfd6bb5d
Charlie <68297416+charles01pd2018@users.noreply.github.com>
2022-11-04 17:17:30 -0700

    remove serialize parsed html

--- src/server.ts
+++ src/server.ts
@@ -130,11 +130,6 @@ export const serializeHTML =
       .serialize(parseHTML(document_)(html));
 }
 
-export const serializeParsedHTML = (html: string) => {
-    return extensions.serializer()
-      .serialize(html);
-}
-
 export const parseMarkdown = (markdown: string) => {
   return markdownParser.parse(markdown);
 };
