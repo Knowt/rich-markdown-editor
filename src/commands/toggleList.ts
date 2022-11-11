9ef2c1a09bd83a3e1a3b85e81a6f2f660c2297fb
Charlie <68297416+charles01pd2018@users.noreply.github.com>
2022-11-10 21:36:38 -0800

    checkpoint: convert checklist to bullet

--- src/commands/toggleList.ts
+++ src/commands/toggleList.ts
@@ -23,18 +23,50 @@ export default function toggleList(listType: NodeType, itemType: NodeType) {
         return liftListItem(itemType)(state, dispatch);
       }
 
-      if (
-        isList(parentList.node, schema) &&
-        listType.validContent(parentList.node.content)
-      ) {
-        const { tr } = state;
-        tr.setNodeMarkup(parentList.pos, listType);
+      if ( isList(parentList.node, schema) ) {
+        if ( listType.validContent(parentList.node.content) ) {
+          const { tr } = state;
+          tr.setNodeMarkup(parentList.pos, listType);
 
-        if (dispatch) {
-          dispatch(tr);
+          if (dispatch) {
+            dispatch(tr);
+          }
+  
+          return false;
         }
+        else if ( parentList.node.type.name === 'checkbox_list' ) {
+          const newParentList = { ...parentList };
+          console.log( 'PARENT CONTENT BEFFORE' );
+          console.log( newParentList.node.content )
 
-        return false;
+          // @ts-ignore
+          const content = parentList.node.content?.content as any[];
+
+          parentList.node.content.content = content.map( ( node ) => {
+            const newItem = state.schema.nodes.list_item.create( 
+              undefined,
+              node.content,
+            );
+
+            return newItem;
+          } );
+
+          console.log( 'PARENT CONTENT AFTER' );
+          console.log( newParentList.node.content );
+
+          const newList = state.schema.nodes.bullet_list.create( 
+            undefined,
+            parentList.node.content,
+          );
+
+          dispatch(
+            state.tr.replaceSelectionWith(
+              newList
+            ),
+          );
+
+          return false;
+        }
       }
     }
 
--- src/nodes/CheckboxItem.ts
+++ src/nodes/CheckboxItem.ts
@@ -8,11 +8,17 @@ export default class CheckboxItem extends Node {
     return "checkbox_item";
   }
 
+  get defaultOptions() {
+    return {
+      softToDOM: false,
+    };
+  }
+
   get schema() {
     return {
       attrs: {
         checked: {
-          default: false
+          default: false,
         }
       },
       content: "paragraph block*",
@@ -54,6 +60,7 @@ export default class CheckboxItem extends Node {
             "data-type": this.name,
             class: node.attrs.checked ? "checked" : undefined
           },
+          this.options.softToDOM ? 0 :
           [
             "span",
             {
