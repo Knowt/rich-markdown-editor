ca0b5a39cd73676c4803794236b9cbb170922f91
Charlie <68297416+charles01pd2018@users.noreply.github.com>
2022-11-12 08:41:05 -0800

    base toggleList

--- src/commands/toggleList.ts
+++ src/commands/toggleList.ts
@@ -1,8 +1,9 @@
-import { NodeType } from "prosemirror-model";
 import { EditorState, Transaction } from "prosemirror-state";
 import { wrapInList, liftListItem } from "prosemirror-schema-list";
 import { findParentNode } from "@knowt/prosemirror-utils";
 import isList from "../queries/isList";
+import { replaceParentNodeOfType } from '@knowt/prosemirror-utils';
+import type { NodeType, Fragment, Node } from "prosemirror-model";
 
 export default function toggleList(listType: NodeType, itemType: NodeType) {
   return (state: EditorState, dispatch: (tr: Transaction) => void) => {
@@ -23,18 +24,55 @@ export default function toggleList(listType: NodeType, itemType: NodeType) {
         return liftListItem(itemType)(state, dispatch);
       }
 
-      if (
-        isList(parentList.node, schema) &&
-        listType.validContent(parentList.node.content)
-      ) {
-        const { tr } = state;
-        tr.setNodeMarkup(parentList.pos, listType);
+      if ( isList(parentList.node, schema) ) {
+        // handles conversion between checklists and other lists
+        if ( !listType.validContent(parentList.node.content) ) {
+          try {
+            // @ts-ignore
+            const content = parentList.node.content?.content as Node[];
+            let newContent: Fragment | undefined = undefined;
 
-        if (dispatch) {
-          dispatch(tr);
+            content.forEach( ( node, index ) => {
+              const contentToUse = newContent || parentList.node.content;
+              const newItem = itemType.create( 
+                undefined,
+                node.content,
+              );
+  
+              newContent = contentToUse.replaceChild(index, newItem);
+            } );
+
+            const newList = listType.create( 
+              undefined,
+              newContent,
+            );
+
+            if ( !newContent ) {
+              throw( new Error( `Could not convert list from ${parentList.node.type.name} to ${listType.name}`) );
+            }
+
+            if ( dispatch ) {
+              dispatch(
+                replaceParentNodeOfType(parentList.node.type, newList)(state.tr)
+              );
+            }
+  
+            return false;
+          }
+          catch ( error ) {
+            console.warn( error?.message );
+          }
         }
+        else {
+          const { tr } = state;
+          tr.setNodeMarkup(parentList.pos, listType);
 
-        return false;
+          if (dispatch) {
+            dispatch(tr);
+          }
+  
+          return false;
+        }
       }
     }
 
