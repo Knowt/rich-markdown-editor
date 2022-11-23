580bcd2cfbba6841455201a89b1a51a604caf3ed
Charlie <68297416+charles01pd2018@users.noreply.github.com>
2022-11-23 14:04:22 -0800

    Revert "isReady"
    This reverts commit 4899ee9665243a3bb1e69a575e53fa1bd82f9493.

--- src/index.tsx
+++ src/index.tsx
@@ -209,7 +209,6 @@ class RichMarkdownEditor extends React.PureComponent<Props, State> {
   marks: { [name: string]: MarkSpec };
   commands: Record<string, any>;
   rulePlugins: PluginSimple[];
-  isReady: boolean;
 
   componentDidMount() {
     this.init();
@@ -227,8 +226,6 @@ class RichMarkdownEditor extends React.PureComponent<Props, State> {
       defaultHighlight,
       defaultBackground,
     } ) );
-
-    this.isReady = true;
   }
 
   componentDidUpdate(prevProps: Props) {
