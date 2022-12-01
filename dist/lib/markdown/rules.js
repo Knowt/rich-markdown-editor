10b7fb26a47b465e117dd54f5ed7666af0c2683a
Charlie <68297416+charles01pd2018@users.noreply.github.com>
2022-12-01 14:53:42 -0800

    bump v + build

--- dist/lib/markdown/rules.js
+++ dist/lib/markdown/rules.js
@@ -6,9 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const markdown_it_1 = __importDefault(require("markdown-it"));
 const BLOCK_RULER_DISABLED = ['code'];
 const FLASHCARD_BLOCK_RULER_DISABLED = ['code', 'list', 'heading', 'lheading', 'table', 'fence', 'blockquote', 'hr'];
-const FLASHCARD_CORE_RULER_DISABLED = ['tables-pm', 'embeds', 'checkboxes', 'linkify', 'emoji', 'smartquotes'];
+const FLASHCARD_CORE_RULER_DISABLED = ['embeds', 'checkboxes', 'linkify', 'emoji', 'smartquotes'];
 const FLASHCARD_INLINE_RULER_DISABLED = ['autolink', 'image', 'link'];
-function rules({ rules = {}, plugins = [], isFlashcardEditor = true, }) {
+function rules({ rules = {}, plugins = [], isFlashcardEditor = false, }) {
     const markdownIt = markdown_it_1.default("default", Object.assign({ breaks: false, html: false, linkify: false }, rules));
     plugins.forEach((plugin) => markdownIt.use(plugin));
     if (isFlashcardEditor) {
