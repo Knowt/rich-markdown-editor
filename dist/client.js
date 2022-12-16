a1b737cd09b51d503980dd3d8492c8ff89415cca
Charlie <68297416+charles01pd2018@users.noreply.github.com>
2022-12-16 10:09:32 -0800

    build

--- dist/client.js
+++ dist/client.js
@@ -37,7 +37,7 @@ const normalizeFlashcardText = (text) => {
             newText += '\n';
         }
         else {
-            newText += exports.cleanFlashcardSpecialChars(trimmedLine) + '\\n';
+            newText += exports.cleanFlashcardSpecialChars(trimmedLine) + '\n';
         }
     }
     return newText;
